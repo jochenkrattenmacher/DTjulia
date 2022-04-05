5076ad48413cd6dd7c9f15e988997d0db069625e
Jochen Krattenmacher <jochen.krattenmacher@web.de>
2022-04-05 21:55:25 +0200

    delete simplesim folder

--- simpleSim/bin/runtask.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\Jochen\AppData\Local\Programs\Julia-1.7.2\bin\julia.exe" --color=yes --depwarn=no --project=@. -q -- "%~dp0..\bootstrap.jl" -r %*
--- simpleSim/bin/server.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\Jochen\AppData\Local\Programs\Julia-1.7.2\bin\julia" --color=yes --depwarn=no --project=@. -q -i -- "%~dp0..\bootstrap.jl" s %*
